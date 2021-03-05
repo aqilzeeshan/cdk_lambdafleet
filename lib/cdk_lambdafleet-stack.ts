4e4c0a1841be39a918911f790cd3cf3ba7473eca
aqilzeeshan <aqilzeeshan@users.noreply.github.com>
2021-03-05 18:17:45 +0000

    add files

--- lib/cdk_lambdafleet-stack.ts
+++ lib/cdk_lambdafleet-stack.ts
@@ -1,9 +1,20 @@
 import * as cdk from '@aws-cdk/core';
+import * as path from 'path'
+
+import * as Lambda from "@aws-cdk/aws-lambda"
+
+import { LambdaFleet } from './LambdaFleet'
 
 export class CdkLambdafleetStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Configure path to Dockerfile
+    const dockerfile = path.join(__dirname, "../src/lambda-node-example");
+
+    // Create AWS Lambda function and push image to ECR
+    new Lambda.DockerImageFunction(this, "function", {
+      code: Lambda.DockerImageCode.fromImageAsset(dockerfile),
+    });
   }
 }
